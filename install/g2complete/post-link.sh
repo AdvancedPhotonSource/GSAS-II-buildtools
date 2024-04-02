e25dabf7c391f0e4b9e81aa05c37e3d98905d185
BHT <toby@anl.gov>
2024-04-02 17:25:31 -0500

    changes that seem to fix build issue where gsas2complete-<ver>-0.tar.bz2 file is not found in install, but it is unclear why.

--- install/g2complete/post-link.sh
+++ install/g2complete/post-link.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
-logfile=/tmp/conda_G2postbuild.log
+logfile=/tmp/G2complete_postlink.log
 echo Preparing to install GSAS-II from GitHub > $logfile
-# create scripts that might be of use for GSAS-II
-# create bootstrap script
-#echo "# Commands to run GSAS-II load/update process" > $CONDA_ROOT/start_G2_bootstrap.sh
-#echo "source $CONDA_ROOT/bin/activate $CONDA_DEFAULT_ENV" >> $CONDA_ROOT/start_G2_bootstrap.sh
-#echo "$PREFIX/bin/python $PREFIX/gitstrap.py" >> $CONDA_ROOT/start_G2_bootstrap.sh
-# create start script
-#echo "# Commands to start GSAS-II" > $CONDA_ROOT/start_GSASII.sh
-#echo "source $CONDA_ROOT/bin/activate $CONDA_DEFAULT_ENV" >> $CONDA_ROOT/start_GSASII.sh
-#echo "$PREFIX/bin/python $PREFIX/GSASII.py" >> $CONDA_ROOT/start_GSASII.sh
-#
-# install and run the bootstrap
-echo fit git files >> $logfile
 mv -v $PREFIX/GSAS-II/keep_git $PREFIX/GSAS-II/.git >> $logfile 2>&1
 mv -v $PREFIX/GSAS-II/keep.gitignore $PREFIX/GSAS-II/.gitignore >> $logfile 2>&1
--- localbuild/G2full_Mac64.sh
+++ localbuild/G2full_Mac64.sh
@@ -6,10 +6,10 @@
 # (cd /tmp; curl -L -O https://github.com/AdvancedPhotonSource/GSAS-II-buildtools/raw/main/localbuild/G2full_Mac64.sh)
 #=======================================================================
 WORKSPACE=/tmp
-rm -rf $WORKSPACE/conda311 $WORKSPACE/builds $WORKSPACE/GSAS2-build $WORKSPACE/GSAS2-code
-
 condaHome=/tmp/conda311
-builds=/tmp/builds
+rm -rf $condaHome
+builds=~/build  # this must match what is in the g2full/construct.yaml file
+
 gitInstallRepo=git@github.com:AdvancedPhotonSource/GSAS-II-buildtools.git
 gitCodeRepo=git@github.com:AdvancedPhotonSource/GSAS-II.git
 
@@ -19,7 +19,7 @@ numpyver=1.26
 packages="python=$pyver wxpython numpy=$numpyver scipy matplotlib pyopengl conda anaconda-client constructor conda-build git gitpython requests pillow h5py imageio scons"
 
 env=bldpy311     # py 3.11.8 & np 1.26.4
-miniforge=https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-MacOSX-x86_64.sh
+miniforge=https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge-MacOSX-x86_64.sh
 
 install=True
 #install=False
@@ -37,19 +37,19 @@ upload=False
 #========== conda stuff
 if [ "$install" = "True" ]
 then
-	if [ ! -e "/tmp/Miniconda3-latest.sh" ]; then
+	if [ ! -e "/tmp/Miniforge-latest.sh" ]; then
 	    echo Downloading 
-	    curl -L $miniforge -o /tmp/Miniconda3-latest.sh
+	    curl -L $miniforge -o /tmp/Miniforge-latest.sh
 	else
 	    echo "skipping miniconda download"
 	fi
 	if [ ! -d "$condaHome" ]; then
 	    echo creating conda installation 
-	    bash /tmp/Miniconda3-latest.sh -b -p $condaHome
+	    bash /tmp/Miniforge-latest.sh -b -p $condaHome
 	else
 	    echo "skip miniconda install"
 	fi
-	#rm /tmp/Miniconda3-latest.sh
+	#rm /tmp/Miniforge-latest.sh
 fi
 
 echo source $condaHome/bin/activate
@@ -96,11 +96,9 @@ then
     echo python `pwd`/setgitversion.py $WORKSPACE/GSAS2-code
     python setgitversion.py $WORKSPACE/GSAS2-code
 
+    rm -rf $builds
     mkdir -p $builds
-    rm -rf $builds/*
     set +x
-    echo conda build purge
-         conda build purge
     echo conda build g2complete --output-folder $builds --numpy $numpyver
          conda build g2complete --output-folder $builds --numpy $numpyver
     set -x    
@@ -110,13 +108,15 @@ then
     # Build the self-installer
     rm -f *.sh
     # constructor g2full
-    # March 2024, mamba solver has a problem, fixed in a newer version
+    # March 2024: mamba solver has a bug, fixed in new version TBA
+    # but not in 3.7.0, following command is a work-around
     CONDA_SOLVER=classic constructor g2full
     set -x
     echo `pwd`
     ls -l *.sh
     mv -v *.sh ../   # put into $WORKSPACE/GSAS2-build
     open ..
+    rm -rf $builds
 	#ls -l *.*
 fi
 
