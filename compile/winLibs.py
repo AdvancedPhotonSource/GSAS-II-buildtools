e028f38d7fc15d3b10693e309e52b9c98286b1c1
BHT <toby@anl.gov>
2024-05-28 00:03:05 -0500

    try all platforms in one swell foop; windows minimize .dlls

--- compile/winLibs.py
+++ compile/winLibs.py
@@ -30,34 +30,24 @@ if __name__ == '__main__':
         s = subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE,
                                  encoding='utf-8')
         out,err = s.communicate()
+        print(f)
         if err:
             print(f,err)
             continue
         for i in out.split('\n')[1:]:
+            print(i)
             if not i: continue
             if '=>' not in i: continue
             items = i.split()
             if len(items) < 3:
                 print(i,'unparsed')
                 continue
-            lib = items[2]
-            libs.add(items[2])
-            continue
-            # if os.path.split(lib)[0] == "/usr/lib": # ignore items in system library (usually libSystem.B.dylib)
-            #     if "libSystem" not in lib: print("ignoring ",lib)
-            #     continue
-            # if "@rpath" in lib and lib not in ignorelist:
-            #     ignorelist.append(lib)
-            #     print("ignoring ",lib)
-            #     continue
-            # elif "/" not in lib and lib not in ignorelist:
-            #     ignorelist.append(lib)
-            #     print("ignoring ",lib)
-            #     continue
-            # if lib not in libs:
-            #     libs[lib] = []
-            # libs[lib].append(f)
+            if 'mingw' in i:
+                libs.add(items[2])
+                print('using',items[2])
+            else:
+                print('ignoring',items[2])
     for key in libs:
-        if 'libgcc' in key or 'libgfortran' in key or 'libquadmath' in key:
+#        if 'libgcc' in key or 'libgfortran' in key or 'libquadmath' in key:
             shutil.copy(key,dirloc)
             print('copying\t',os.path.split(key)[1],'to',dirloc)
