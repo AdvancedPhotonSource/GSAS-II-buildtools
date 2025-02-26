5fffeef9eb5b51dd50670971ab33b6a291e294c4
BHT <toby@anl.gov>
2025-02-26 17:06:55 -0600

    .exe crashing before completion; move shortcut & gitstrap to conda

--- install/GSAS2new/g2postinstall.bat
+++ install/GSAS2new/g2postinstall.bat
@@ -1,24 +1,4 @@
 REM ======================================================================
-echo "Finish up GSAS-II installation"
+REM echo "Finish up GSAS-II installation"
+REM final steps in GSAS-II installation are now in post-link.bat (conda package)
 REM ======================================================================
-set python=%PREFIX%\python.exe
-set gitstrap=%PREFIX%\gitstrap.py
-set logfile=%PREFIX%\g2post_out.log
-set gitlog1=%PREFIX%\gitstrap1.log
-set gitlog2=%PREFIX%\gitstrap2.log
-REM ======================================================================
-echo %python% %gitstrap% --nocheck --log=%gitlog1% --noshortcut >> %logfile%
-     %python% %gitstrap% --nocheck --log=%gitlog1% --noshortcut >> %logfile%
-REM finish installation
-echo %python% %gitstrap% --nocheck --log=%gitlog2% --nodownload >> %logfile%
-     %python% %gitstrap% --nocheck --log=%gitlog2% --nodownload >> %logfile%
-REM create bootstrap batch file ================================================
-echo REM Commands to run GSAS-II load/update process > "%PREFIX%\G2_bootstrap.bat"
-echo call %PREFIX%\Scripts\activate >> "%PREFIX%\G2_bootstrap.bat"
-echo python %gitstrap% >> "%PREFIX%\G2_bootstrap.bat"
-REM create shortcut to start GSAS-II     =======================================
-echo REM Commands to run GSAS-II load/update process > "%PREFIX%\G2_start.bat"
-echo call %PREFIX%\Scripts\activate >> "%PREFIX%\G2_start.bat"
-echo python %PREFIX%\GSAS-II\GSASII\G2.py >> "%PREFIX%\G2_start.bat"
-set errorlevel=0
-exit 0
--- install/GSAS2new/post-link.bat
+++ install/GSAS2new/post-link.bat
@@ -12,7 +12,18 @@ if errorlevel 1 exit 1
 
 del git.tgz >> %logfile%
 REM
-REM This does not run gitstrap.py here because it is assumed that will
-REM happen later when the conda constructor installer runs. If this package
-REM is ever to be used independently, it should be run here.
-REM likewise, see g2postinstall.bat for commands that 
+REM create shortcut to start GSAS-II     =======================================
+echo REM Commands to run GSAS-II load/update process > "%PREFIX%\G2_start.bat"
+echo call %PREFIX%\Scripts\activate >> "%PREFIX%\G2_start.bat"
+echo python %PREFIX%\GSAS-II\GSASII\G2.py >> "%PREFIX%\G2_start.bat"
+REM create bootstrap batch file ================================================
+echo REM Commands to run GSAS-II load/update process > "%PREFIX%\G2_bootstrap.bat"
+echo call %PREFIX%\Scripts\activate >> "%PREFIX%\G2_bootstrap.bat"
+echo python %gitstrap% >> "%PREFIX%\G2_bootstrap.bat"
+REM
+echo %python% %gitstrap% --nocheck --log=%gitlog1% --noshortcut >> %logfile%
+     %python% %gitstrap% --nocheck --log=%gitlog1% --noshortcut >> %logfile%
+REM finish installation
+echo %python% %gitstrap% --nocheck --log=%gitlog2% --nodownload >> %logfile%
+     %python% %gitstrap% --nocheck --log=%gitlog2% --nodownload >> %logfile%
+echo post-link.bat now done >> %logfile%
