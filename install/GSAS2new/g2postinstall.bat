ce86520cc1a63b7b13df14ba852bef5b509721b9
BHT <toby@anl.gov>
2025-02-27 17:57:22 -0600

    restore windows build steps, but now with constructor=3.9

--- install/GSAS2new/g2postinstall.bat
+++ install/GSAS2new/g2postinstall.bat
@@ -1,16 +1,15 @@
 REM ======================================================================
-REM echo "Finish up GSAS-II installation"
-REM final steps in GSAS-II installation are now in post-link.bat (conda package)
+REM Finish up GSAS-II installation by trying to update GSAS-II 
+REM and create a shortcut
 REM ======================================================================
-exit
 
-REM
 set python=%PREFIX%\python.exe
 set gitstrap=%PREFIX%\gitstrap.py
 set logfile=%PREFIX%\g2post_out.log
 set gitlog1=%PREFIX%\gitstrap1.log
 set gitlog2=%PREFIX%\gitstrap2.log
 REM
+echo Updating GSAS-II with gitstrap.py > %logfile%
 echo %python% %gitstrap% --nocheck --log=%gitlog1% --noshortcut >> %logfile%
      %python% %gitstrap% --nocheck --log=%gitlog1% --noshortcut >> %logfile%
 REM finish installation
--- install/GSAS2new/post-link.bat
+++ install/GSAS2new/post-link.bat
@@ -1,15 +1,12 @@
-set logfile=%PREFIX%\g2complete_postlink.log
+set logfile=%PREFIX%\g2new_postlink.log
 echo Completing install of GSAS-II from conda package > %logfile%
 REM 
 REM ============= Restore the git repository file
 REM 
-dir  >> %logfile%
-echo fix git files >> %logfile%
+REM dir  >> %logfile%
+echo restore git files >> %logfile%
 cd %PREFIX%\GSAS-II
-dir  >> %logfile%
-echo post-link.bat now done >> %logfile%
-exit
-
+REM dir  >> %logfile%
 
 echo C:\Windows\System32\tar.exe xzf git.tgz  >> %logfile%
      C:\Windows\System32\tar.exe xzf git.tgz  >> %logfile%
@@ -22,11 +19,6 @@ echo REM Commands to run GSAS-II load/update process > "%PREFIX%\G2_start.bat"
 echo call %PREFIX%\Scripts\activate >> "%PREFIX%\G2_start.bat"
 echo python %PREFIX%\GSAS-II\GSASII\G2.py >> "%PREFIX%\G2_start.bat"
 REM create bootstrap batch file ================================================
-Note that these commands assume that gcc and gfortran are already installed. echo REM Commands to run GSAS-II load/update process > "%PREFIX%\G2_bootstrap.bat"
+echo REM Commands to run GSAS-II load/update process > "%PREFIX%\G2_bootstrap.bat"
 echo call %PREFIX%\Scripts\activate >> "%PREFIX%\G2_bootstrap.bat"
-echo python %gitstrap% >> "%PREFIX%\G2_bootstrap.bat"
-echo PREFIX = %PREFIX%  >> %logfile%
-echo PKG_NAME = %PKG_NAME% >> %logfile%
-echo PKG_VERSION = %PKG_NAME% >> %logfile%
-echo PKG_BUILDNUM = %PKG_BUILDNUM% >> %logfile%
 echo post-link.bat now done >> %logfile%
