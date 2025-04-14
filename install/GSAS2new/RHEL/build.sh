d6d9ec524ec177ce6a2375033d8c2f66c6aab7a7
BHT <toby@anl.gov>
2025-04-14 08:05:34 -0500

    add ignored files

---
+++ install/GSAS2new/RHEL/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# based on output from makeBldFiles.py and then edited manually
+echo '==================== running build.sh ===================='
+logfile=/tmp/conda_G2build_out.log
+#
+echo starting Build.sh > $logfile 2>&1
+cp $RECIPE_DIR/../gitcompile.py $PREFIX/
+echo ls $PREFIX/ >> $logfile 2>&1
+echo git= >> $logfile 2>&1
+which git >> $logfile 2>&1
+echo python= >> $logfile 2>&1
+which python >> $logfile 2>&1
+echo python $PREFIX/gitcompile.py --nocheck --noshortcut --noprogress --log=/tmp/gitcompile.log --branch=main >> $logfile 2>&1
+     python $PREFIX/gitcompile.py --nocheck --noshortcut --noprogress --log=/tmp/gitcompile.log --branch=main >> $logfile 2>&1
+# rename the .git files so they get copied into the conda package
+mv -v $PREFIX/GSAS-II/.git $PREFIX/GSAS-II/keep_git  >> $logfile 2>&1
+mv -v $PREFIX/GSAS-II/.gitignore $PREFIX/GSAS-II/keep.gitignore  >> $logfile 2>&1
