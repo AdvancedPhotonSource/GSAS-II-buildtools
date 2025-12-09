3aea9244b9708f2a9077e47c5b8ca5b6f094b8c6
BHT <toby@anl.gov>
2025-12-09 08:55:07 -0600

    update MacOS .so patch for libquadmath

--- compile/macRelink.py
+++ compile/macRelink.py
@@ -26,39 +26,42 @@ if __name__ == '__main__':
     libs = {}
     ignorelist = []
     print('Scanning for referenced libraries')
-    while fileList:
-        f = fileList.pop(0)
-#    for f in fileList:
+    for f in fileList:
         cmd = ['otool','-L',f]
         s = subprocess.Popen(cmd,encoding='UTF-8',
                     stdout=subprocess.PIPE,stderr=subprocess.PIPE)
         out,err = s.communicate()
-        print('\n\nprocessing=',f,'\nout=',out,'\n')   ##########
+        print('\n\nprocessing=',f)
         for i in out.split('\n')[1:]: 
             if not i: continue
             lib = i.split()[0]
-            print('library=',lib)
             if os.path.split(lib)[0] == "/usr/lib": # ignore items in system library (usually libSystem.B.dylib)
                 if "libSystem" not in lib: print("ignoring ",lib)
                 continue
             if "@rpath" in lib and lib not in ignorelist:
                 ignorelist.append(lib)
-                print("@rpath ignoring ")
+                print("@rpath ignoring ",lib)
                 continue
             elif "/" not in lib and lib not in ignorelist:
                 ignorelist.append(lib)
-                print("no / ignoring ")
+                print("no / ignoring ",lib)
                 continue
             if os.path.split(lib)[1] == os.path.split(f)[1]:
                 continue  # don't act on reference to current file
             if lib not in libs:
-                print('copying')
+                print('copying',lib)
                 libs[lib] = []
                 if os.path.exists(lib):
                     newname = os.path.join(dirloc,os.path.split(lib)[1])
                     shutil.copy(lib,newname)
                     print('library copied',lib,'to',newname)
                     fileList.append(newname)
+                    if 'libgfortran' in lib:
+                        # libquadmath needed by libgfortran
+                        g = os.path.join(os.path.split(lib)[0],'*libquadmath*')
+                        for f1 in glob.glob(g):
+                            shutil.copy(f1,dirloc)
+                            print('library copied',f1,'to',dirloc)
                 else:
                     print('library not found',lib)
                 libs[lib].append(f)
@@ -67,10 +70,10 @@ if __name__ == '__main__':
     for key in libs:
         newkey = os.path.join('@rpath',os.path.split(key)[1])
         print('Fixing',key,'to',newkey)
-        if os.path.exists(key):
-            shutil.copy(key,dirloc)
-        else:
-            print('skipping copy of library',key)
+        # if os.path.exists(key):
+        #     shutil.copy(key,dirloc)
+        # else:
+        #     print('skipping copy of library',key)
         for f in libs[key]:
             print('\t in',os.path.split(f)[1])
             cmd = ["install_name_tool","-change",key,newkey,f]
