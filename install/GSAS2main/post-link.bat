4f4ad58c0bc35af067e14d57b7ef7977860b9948
BHT <toby@anl.gov>
2026-02-07 20:17:54 -0600

    fix errors in .bat file created by conda installer

--- install/GSAS2main/post-link.bat
+++ install/GSAS2main/post-link.bat
@@ -16,14 +16,13 @@ del /s git.tgz >> %logfile%
 REM
 REM create shortcut to start GSAS-II     =======================================
 echo REM Commands to start GSAS-II > "%PREFIX%\G2_start.bat"
-echo call %PREFIX%\condabin\conda.bat >> "%PREFIX%\G2_start.bat"
+echo call %PREFIX%\condabin\conda.bat activate >> "%PREFIX%\G2_start.bat"
 echo call conda activate %PREFIX% >> "%PREFIX%\G2_start.bat"
 echo python %PREFIX%\GSAS-II\GSASII\G2.py >> "%PREFIX%\G2_start.bat"
 REM create bootstrap batch file ================================================
 echo REM Commands to run GSAS-II load/update process > "%PREFIX%\G2_bootstrap.bat"
-echo call %PREFIX%\condabin\conda.bat >> "%PREFIX%\G2_bootstrap.bat"
+echo call %PREFIX%\condabin\conda.bat activate >> "%PREFIX%\G2_bootstrap.bat"
 echo call conda activate %PREFIX% >> "%PREFIX%\G2_bootstrap.bat"
-echo call %PREFIX%\Scripts\activate >> "%PREFIX%\G2_bootstrap.bat"
 echo curl -L -o %PREFIX%\gitstrap.py https://github.com/AdvancedPhotonSource/GSAS-II-buildtools/raw/main/install/gitstrap.py >> "%PREFIX%\G2_bootstrap.bat
 echo python %PREFIX%\gitstrap.py --reset  >> "%PREFIX%\G2_bootstrap.bat
 echo python %PREFIX%\gitstrap.py --nodownload  >> "%PREFIX%\G2_bootstrap.bat
